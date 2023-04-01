fd362d66bbc1c00cc73ef00544057cddffb5a9c8
CameronStClair95 <cameronaitcheson@gmail.com>
2023-04-01 15:59:04 +0100

    Working on Art Post Crud. Mainly the UD

--- src/components/ArtpostCard.jsx
+++ src/components/ArtpostCard.jsx
@@ -1,32 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
-function ArtPostCard({ artpostId }) {
-  const [artpost, setArtpost] = useState(null);
-  const API_URL = 'http://localhost:5005';
-  /* console.log(artpostId); */
-
-  useEffect(() => {
-    axios
-      .get(`${API_URL}/api/posts/artposts/${artpostId}`)
-      .then((response) => {
-        setArtpost(response.data);
-      })
-      .catch((error) => {
-        console.log('Error fetching artpost:', error);
-      });
-  }, [artpostId]);
-
+function ArtPostCard({ artpost }) {
   if (!artpost) {
     return <div>Loading...</div>;
   }
 
   const { artist, title, description, medium, year, dimensions, art_image, author } = artpost;
+  const artpostId = artpost._id; // Get the artpostId from the artpost object
 
   return (
-    <Link className='artpost_card_link'>
-
+    <Link to={`/artposts/${artpostId}`} className='artpost_card_link'>
       <div className="artpost_card">
         <div>
           <img className="art-image" src={art_image} alt={`${title} by ${artist}`} />
@@ -38,7 +22,6 @@ function ArtPostCard({ artpostId }) {
           </h5>
           <h6>{medium}</h6>
           {author && <p>added by {author}</p>}
-        
         </div>
       </div>
     </Link>
@@ -46,4 +29,3 @@ function ArtPostCard({ artpostId }) {
 }
 
 export default ArtPostCard;
-
