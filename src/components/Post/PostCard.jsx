50df3e3e085efc20b1a2b8f511eb0c8c9cdc38d1
CameronStClair95 <cameronaitcheson@gmail.com>
2023-04-08 15:42:40 +0100

    Fixed the Update and delete on the art post and post as well as the img upload

--- src/components/Post/PostCard.jsx
+++ src/components/Post/PostCard.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect, useContext } from "react";
 import axios from "axios";
-import { Button } from "react-bootstrap";
+import { Button, Modal, Form } from "react-bootstrap";
 import { Link, useNavigate, useLocation, useParams } from "react-router-dom";
 import { AuthContext } from "../../context/Auth.context";
-import PostCSS from "./Post.module.css"
+import PostCSS from "./Post.module.css";
 
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import FavoriteIcon from "@mui/icons-material/Favorite";
@@ -11,32 +11,52 @@ import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
 import EditIcon from "@mui/icons-material/Edit";
 import ReadMoreIcon from "@mui/icons-material/ReadMore";
 
-function PostCard({content, place, post_image, postId, getAllPosts, likedBy, author}) {
+function PostCard({ content, place, post_image, postId, getAllPosts, likedBy, author }) {
   const [post, setPost] = useState(null);
   const API_URL = "http://localhost:5005";
   const { user } = useContext(AuthContext);
-
   const navigate = useNavigate();
   const { pathname } = useLocation();
+
+  const [showUpdateForm, setShowUpdateForm] = useState(false);
+  const [updatedContent, setUpdatedContent] = useState(content);
+  const [updatedPlace, setUpdatedPlace] = useState(place);
+  const [updatedPostImage, setUpdatedPostImage] = useState(post_image);
+  const [showDeleteConfirmation, setShowDeleteConfirmation] = useState(false);
+
+  const handleContentChange = (e) => setUpdatedContent(e.target.value);
+  const handlePlaceChange = (e) => setUpdatedPlace(e.target.value);
+  const handlePostImageChange = (e) => setUpdatedPostImage(e.target.value);
+
+  const handleUpdate = (e) => {
+    e.preventDefault();
+    axios.put(`${API_URL}/api/posts/${postId}`, {
+      content: updatedContent,
+      place: updatedPlace,
+      post_image: updatedPostImage
+    })
+    .then((response) => {
+      console.log("Post updated successfully:", response.data);
+      setPost(response.data);
+      setShowUpdateForm(false);
+    })
+    .catch((error) => {
+      console.error("Error updating post:", error);
+    });
+  };
 
   useEffect(() => {
     axios.get(`${API_URL}/api/posts/posts/${postId}`)
       .then((response) => {
-        
-        setPost(response.data)
-        /* console.log("author is", author._id)
-        console.log(response.data.post.author._id, "post author id")
-        console.log(user._id, "user id") */
+        setPost(response.data);
       })
-      .catch((error) => console.error("an error trying to set info", error));
+      .catch((error) => console.error("An error occurred trying to set info", error));
   }, [postId]);
 
   function handleLike() {
-    /* console.log("handle like"); */
     axios.post(`${API_URL}/api/posts/like/${postId}/post`, user)
       .then((response) => {
-        setPost(response.data)
-        /* console.log(response.data) */
+        setPost(response.data);
       })
       .catch((error) => console.error(error));
   }
@@ -46,7 +66,6 @@ function PostCard({content, place, post_image, postId, getAllPosts, likedBy, aut
     axios.delete(`${API_URL}/api/posts/${postId}`)
       .then((response) => {
         console.log("Post deleted successfully:", response.data);
-        /* getAllPosts(); */
         navigate("/home"); // Navigate to the home page after successful deletion
       })
       .catch((error) => {
@@ -54,9 +73,14 @@ function PostCard({content, place, post_image, postId, getAllPosts, likedBy, aut
       });
   };
 
+  const confirmDelete = () => {
+    handleDelete();
+    setShowDeleteConfirmation(false);
+  };
+
   return (
     <div className={PostCSS.post_card}>
-    {author && (<p style={{ fontSize: "small"}}>{author.username}</p>)}
+      {author && (<p style={{ fontSize: "small"}}>{author.username}</p>)}
       <div>
         <img className={PostCSS.post_image} src={post_image}/>
       </div>
@@ -69,13 +93,13 @@ function PostCard({content, place, post_image, postId, getAllPosts, likedBy, aut
       {pathname === "/home" ? (
         <Link to={`/posts/posts/${postId}`} className="post_card_link">
           <Button><ReadMoreIcon/> More</Button>
-        </Link>
+          </Link>
       ) : (
         <>
-        { user?._id === author?._id && 
+        { user?._id === author?._id &&
           <>
-          <Button>Update<EditIcon/></Button>
-          <Button variant="danger" onClick={handleDelete}>Delete <DeleteOutlineIcon/></Button>
+          <Button onClick={() => setShowUpdateForm(true)}>Update<EditIcon/></Button>
+          <Button variant="danger" onClick={() => setShowDeleteConfirmation(true)}>Delete <DeleteOutlineIcon/></Button>
           </>
         }
         </>
@@ -88,7 +112,72 @@ function PostCard({content, place, post_image, postId, getAllPosts, likedBy, aut
           <FavoriteIcon />
         )}
       </button>
-      
+
+      <Modal
+        show={showUpdateForm}
+        onHide={() => setShowUpdateForm(false)}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Update Post</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form onSubmit={handleUpdate}>
+            <Form.Group controlId="formContent">
+              <Form.Label>Content:</Form.Label>
+              <Form.Control
+                type="text"
+                value={updatedContent}
+                onChange={handleContentChange}
+              />
+            </Form.Group>
+
+            <Form.Group controlId="formPlace">
+              <Form.Label>Place:</Form.Label>
+              <Form.Control
+                type="text"
+                value={updatedPlace}
+                onChange={handlePlaceChange}
+              />
+            </Form.Group>
+
+            <Form.Group controlId="formPostImage">
+              <Form.Label>Post Image URL:</Form.Label>
+              <Form.Control
+                type="text"
+                value={updatedPostImage}
+                onChange={handlePostImageChange}
+              />
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
+              Update
+            </Button>
+          </Form>
+        </Modal.Body>
+      </Modal>
+
+      <Modal
+        show={showDeleteConfirmation}
+        onHide={() => setShowDeleteConfirmation(false)}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Delete Post</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          Are you sure you want to delete this post?
+        </Modal.Body>
+        <Modal.Footer>
+          <Button
+            variant="secondary"
+            onClick={() => setShowDeleteConfirmation(false)}
+          >
+            Close
+          </Button>
+          <Button variant="danger" onClick={handleDelete}>
+            Delete
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 }
