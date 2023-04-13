48509119c9ad97a63279be80c966ec5bb130d1df
Maksym Kopychanskyi <kopy.chan.max@gmail.com>
2023-04-13 20:32:08 +0100

    fixed the mispelling

--- src/context/Auth.context.js
+++ src/context/Auth.context.js
@@ -16,7 +16,7 @@ function AuthProviderWrapper(props){
         const storedToken = localStorage.getItem("authToken")
 
         if (storedToken){
-            .geaxiost(`${API_URL}/auth/verify`, {headers: {Authorization: `Bearer ${storedToken}`}})
+            axios.get(`${API_URL}/auth/verify`, {headers: {Authorization: `Bearer ${storedToken}`}})
                 .then((response) => {
                     const user = response.data
                     setIsLoggedIn(true);
